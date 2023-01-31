f235a1f7d1a9b4531d00402f928826440d5dbcff
Brad Williams <bwilliams@c20g.com>
2023-01-31 16:38:32 -0500

    step 2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,7 +1,14 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
-// import org.springframework.data.repository.JpaRepository;
 
-public interface PostRepository extends org.springframework.data.jpa.repository.JpaRepository<Post, Long>{
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+        
+        List<Post> findByCategory(Category category);
+
 }
